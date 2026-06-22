0e858237955e1a4771daa3180e162f4c05c865a2
Marcus Lochard <43730027+mlochard@users.noreply.github.com>
2026-06-21 20:38:58 -0400

    Delete scripts directory

--- scripts/compare-ca-policies.ps1
+++
@@ -1,4 +0,0 @@
-$old = Get-Content "./backup-old.json" | ConvertFrom-Json
-$new = Get-Content "./backup-new.json" | ConvertFrom-Json
-
-Compare-Object $old $new -Property displayName, state, conditions, grantControls
